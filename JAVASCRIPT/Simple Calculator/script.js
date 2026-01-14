b0807f3d0e0938eebf8031ea45a3694bc8da34cd
Rudra Pratap Singh <rpsingh7672@gmail.com>
2026-01-15 01:00:04 +0530

    Create script.js

---
+++ JAVASCRIPT/Simple Calculator/script.js
@@ -0,0 +1,17 @@
+let a= 10;
+let b= 5;
+function add(){
+    alert("The addtion of 10 and 5 is :" +(a+b));
+}
+function sub(){
+    alert("The substraction of 10 and 5 is :" +(a-b));
+}
+function mul(){
+    alert("The multiplication of 10 and 5 is :" +(a*b));
+}
+function divi(){
+    alert("The division of 10 and 5 is :" +(a/b));
+}
+function modu(){
+    alert("The modulos of 10 and 5 is :" +(a%b));
+}
